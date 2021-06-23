480a3e736561c81a62d1d38baedf31fc5a47de5b
David Giese <johndgiese@gmail.com>
2021-06-22 22:34:17 -0400

    Add basic email form

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def hello_world():
-    return render_template("form.html")
+    if request.method == 'GET':
+        return render_template("form.html")
+    elif request.method == 'POST':
+        print(request.form['name'], request.form['email'])
+        return render_template("form.html")
